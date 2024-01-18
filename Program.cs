0f71c4931133b4bd0761ef658f89376a65e20252
zachhansen <zachhansen@ZACHHANSENE3CB>
2024-01-17 19:58:22 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Mission2;
+using System;
+using System.Runtime.InteropServices;
+using System.Xml;
+// Zach Hansen
+// Mission2 assignment
+// Section 001
+// Create program class
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // create function to do random
+        Random random = new Random();
+        // set variables
+        int NumOfRolls;
+        
+        //write to the console asking for how many rolls and getting their input
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        NumOfRolls = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each * represents 1% of the total number of rolls."); 
+        System.Console.WriteLine("Total number of rolls = " + NumOfRolls + "\n");
+
+        //pull from the RollDie class
+        RollDice rolldice = new RollDice();
+        //put those dice rolls into an array
+        int[] result = rolldice.rollTheDice(NumOfRolls);
+
+        // for loop to output the results of the rolls
+        //for (int i = 0; i < result.Length; i++)
+        //{
+        //    System.Console.WriteLine($" {i + 2}: {result[i]}");
+        //}
+        //call the histogram function
+        Histogram(result, NumOfRolls);
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+    //Function to write and display the hisogram
+    static void Histogram(int[] result, int NumOfRolls)
+    {
+        //Write histogram labels
+        Console.WriteLine("Total\tFrequency\tPercentage");
+        Console.WriteLine("---------------------------------");
+
+        //for loop to go through the results and output the histogram with each * = to 1%
+        for (int i = 0; i < result.Length; i++)
+        {
+            double percentage = (double)result[i] / NumOfRolls * 100;
+            int asterisks = (int)Math.Round(percentage);
+
+            Console.Write($"{i + 2}\t{result[i]}\t\t");
+
+            for (int j = 0; j < asterisks; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine($" ({percentage:F2}%)");
+        }
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    //create class to roll the dice
+    public class RollDice
+    {
+        //this with generate the 2 dice rolls and sum them up
+        public int[] rollTheDice(int NumOfRolls)
+        {
+            // create function to do random
+            Random random = new Random();
+            //make an array to store the total number of roll sums
+            int[] rolltotals = new int[11];
+
+            // for loop to through and roll the two dice and get their sum then add them to the array
+            for (int rollNumber = 1; rollNumber <= NumOfRolls; rollNumber++)
+            {
+                int dice1 = RollResult(random);
+                int dice2 = RollResult(random);
+                int diceSum = dice1 + dice2;
+
+                rolltotals[diceSum - 2]++;
+            }
+            return rolltotals;
+        }
+        //set the random function
+        static int RollResult(Random random)
+        {
+            return random.Next(1, 7);
+        }
+    }
+}
